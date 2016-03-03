d10e7ca8fb7a92cb4a362797eb458fb9ccfc59a5
cartagena <jcartagena93@gmail.com>
2016-03-02 21:31:16 -0500

    adds followers association for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ActiveRecord::Base
   has_many :active_relationships, class_name: "Relationship",
                                   foreign_key: "follower_id",
                                   dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   has_secure_password
 
   validates :name, presence: true, length: { maximum: 50 }
