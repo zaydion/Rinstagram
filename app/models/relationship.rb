d1cf7a0ed4267b41b50a0e43a18e8ffb35e8ed27
cartagena <jcartagena93@gmail.com>
2016-03-02 21:21:34 -0500

    builds user && relationships associations

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,6 @@
 class Relationship < ActiveRecord::Base
+    has_many :follower, class_name: "User"
+    has_many :followed, class_name: "User"
+    validates :follower_id, presence: true
+    validates :followed_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
   has_many :posts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
   has_secure_password
 
   validates :name, presence: true, length: { maximum: 50 }
